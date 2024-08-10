61d9fa4f454ecf25b722cae12f610d27004a0309
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-08-10 11:00:24 +0200

    Update DeliverToMe.ps1

--- .github/DeliverToMe.ps1
+++ .github/DeliverToMe.ps1
@@ -1,5 +1,7 @@
 Param(
     [Hashtable]$parameters
 )
-
 Get-ChildItem -Path $parameters.appsFolder | Out-Host
+$context = $parameters.context | ConvertFrom-Json
+Write-Host $context
+Write-Host "Password Length: $($context.Password.Length)"
