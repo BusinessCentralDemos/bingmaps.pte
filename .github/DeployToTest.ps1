80f46d9662192cea3503408fc2fca4817455d8df
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-08-10 11:26:13 +0200

    Update DeployToTest.ps1

--- .github/DeployToTest.ps1
+++ .github/DeployToTest.ps1
@@ -5,3 +5,6 @@ $tempPath = Join-Path ([System.IO.Path]::GetTempPath()) ([GUID]::NewGuid().ToStr
 New-Item -ItemType Directory -Path $tempPath | Out-Null
 Copy-AppFilesToFolder -appFiles $parameters.apps -folder $tempPath | Out-Null
 Get-ChildItem -Path $tempPath -Filter *.app | Out-Host
+$authContext = $parameters.authContext | ConvertFrom-Json
+Write-Host $authContext
+Write-Host "Password Length: $($authContext.Password.Length)"
